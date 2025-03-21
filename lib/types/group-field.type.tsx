1ac15bebcca8db5989f4cee48e90bff554a93651
ismet kizgin <info@ismetkizgin.com.tr>
2025-03-21 22:51:22 +0300

    fix: fix for group field model

--- lib/types/group-field.type.tsx
+++ lib/types/group-field.type.tsx
@@ -5,11 +5,15 @@ export class GroupField {
   fields: Array<FieldType>;
   containerVisible?: boolean;
   containerOptions?: Record<string, any>;
+  useContainersOutsideGroup?: boolean;
+  useGroupContainer?: boolean;
 
   constructor(options: GroupField) {
     this.id = options.id;
     this.fields = options.fields ?? [];
     this.containerVisible = options.containerVisible ?? false;
+    this.useContainersOutsideGroup = options.useContainersOutsideGroup ?? false;
+    this.useGroupContainer = options.useGroupContainer ?? false;
     this.containerOptions = options.containerOptions ?? {};
   }
 }
