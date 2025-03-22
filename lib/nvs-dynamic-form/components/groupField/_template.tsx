8fa694e54bc1db66ce20ab4f283685d2bcf4b5e1
ismet kizgin <info@ismetkizgin.com.tr>
2025-03-23 01:53:10 +0300

    fix: fix for group field

--- lib/nvs-dynamic-form/components/groupField/_template.tsx
+++ lib/nvs-dynamic-form/components/groupField/_template.tsx
@@ -1,7 +1,8 @@
-import React, { useEffect, useState } from "react";
+import * as lodash from "lodash";
+
+import React, { useState } from "react";
 
 import { Elements } from "../elements";
-import { FieldType } from "../../_type";
 import { FormBuilder } from "../formBuilder";
 import { IGroupField } from "./_type";
 
@@ -14,16 +15,12 @@ export const GroupField = ({
   useGroupContainer,
   buttonComponent,
 }: IGroupField) => {
-  const [fields, setFields] = useState<Array<FieldType>>();
-
-  useEffect(() => {
-    setFields(
-      groupField.fields.map((field) => {
-        field.id = `${groupField.id}.${field.id}`;
-        return field;
-      })
-    );
-  }, [groupField.fields]);
+  const [fields, _] = useState(
+    lodash.cloneDeep(groupField.fields).map((field) => {
+      field.id = `${groupField.id}.${field.id}`;
+      return field;
+    })
+  );
 
   const isContainerVisible = () => {
     return groupField.containerVisible && useGroupContainer && containerVisible;
